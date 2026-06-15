bed3d0868740d823253b09c340c16d0da4cb1551
Julian <julishalo99@hotmail.com>
2026-06-15 15:10:48 -0500

    feat: login por medio de clerk

---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,14 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function SignUpPage() {
+  return (
+    <div style={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      minHeight: "100vh",
+    }}>
+      <SignUp />
+    </div>
+  );
+}
